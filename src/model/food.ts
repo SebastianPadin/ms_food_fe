e9b5c069396e676e34658fe396a880e5c766c7b3
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-06 12:19:45 -0500

    Update food.ts

--- src/model/food.ts
+++ src/model/food.ts
@@ -1,5 +1,5 @@
 export interface Food {
-    id_food: number; 
+    idFood: number; 
     foodType: string; 
     foodBrand: string; 
     amount: string; 
@@ -16,7 +16,7 @@ export interface Food {
     unitMeasure: string; 
   }
   export interface FoodUpdate {
-    id_food: number; 
+    idFood: number; 
     foodType: string; 
     foodBrand: string; 
     amount: string; 
