783f6bef793fedb6c04b421e1599d4cde49b1f61
Sebastian Pablo Padin Munayco <sebastian.padin@vallegrande.edu.pe>
2025-07-10 23:15:09 +0000

    a

--- src/tests/egg.spec.ts
+++ src/tests/egg.spec.ts
@@ -9,20 +9,20 @@ test('Prueba simplificada de agregar registro de producción de huevos', async (
     console.log(`Clic en: ${element}`);
   };
 
-  // 1. Ir a la página
-  await page.goto('http://localhost:4200/Modulo-Galpon/Producción%20de%20huevos', { timeout: 60000 });
+  // 1. Ir a la página 
+  await page.goto('http://localhost:4200/Modulo-Galpon/Producci%C3%B3n%20de%20huevos', { timeout: 60000 });
 
-  // 2. Abrir el modal
-  const abrirModalButton = page.locator('button.bg-green-500:has-text("Agregar Producción")');
+  // 2. Abrir el modal 
+  const abrirModalButton = page.getByRole('button', { name: /Ingresar Producción/i });
   await abrirModalButton.waitFor({ state: 'visible', timeout: 30000 });
   await abrirModalButton.click();
-  logClick('Abrir Modal Agregar Producción');
+  logClick('Abrir Modal Ingresar Producción');
 
-  // Esperar a que el modal esté visible (asegurar que esté abierto)
+  // 3. Esperar a que el modal esté visible
   const modal = page.locator('div:has(h3:has-text("Nuevo Registro de Producción"))');
   await modal.waitFor({ state: 'visible', timeout: 10000 });
 
-  // 3. Llenar el formulario
+  // 4. Llenar el formulario
   await page.fill('#quantityEggs', '120');
   logClick('Cantidad de Huevos');
 
@@ -36,13 +36,13 @@ test('Prueba simplificada de agregar registro de producción de huevos', async (
   await page.fill('#registrationDate', fechaActual);
   logClick('Fecha de registro');
 
-  // 4. Enviar el formulario
-  const enviarFormularioButton = page.locator('button[type="submit"]:has-text("Guardar")');
+  // 5. Enviar el formulario
+  const enviarFormularioButton = page.getByRole('button', { name: /Guardar/i });
   await enviarFormularioButton.waitFor({ state: 'visible', timeout: 10000 });
   await enviarFormularioButton.click();
   logClick('Guardar');
 
-  // 5. Finalizar test
+  // 6. Finalizar test
   const endTime = performance.now();
   const duration = (endTime - startTime) / 1000;
 
